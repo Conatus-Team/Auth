a920f9990dfbc3698cd98a2d64a09a8a8c5cec30
Jin Nam <developsick@gmail.com>
2022-08-25 00:31:43 +0900

    edit properties

--- src/main/java/moine/config/kafka/KafkaProcessor.java
+++ src/main/java/moine/config/kafka/KafkaProcessor.java
@@ -8,13 +8,14 @@ import org.springframework.messaging.SubscribableChannel;
 public interface KafkaProcessor {
     // 발행, 구독 토픽 이름 설정
     String INPUT = "event-in"; // 구독하는 토픽
-//    String OUTPUT = "event-out";
-    String OUTPUT1 = "authTopic"; // 발행할 토픽
+    String OUTPUT = "event-out";
+//    String OUTPUT1 = "authTopic"; // 발행할 토픽
 
     @Input(INPUT)
     SubscribableChannel inboundTopic();
 
-    @Output(OUTPUT1)
+//    @Output(OUTPUT1)
+    @Output(OUTPUT)
     MessageChannel outboundTopic();
 
 }
