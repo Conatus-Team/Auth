e0e473710f73aa12c6e3c6632309f363df391323
KimRiun <kimriun27@gmail.com>
2022-08-27 11:49:47 +0900

    Auto stash before merge of "develop2/kafka" and "origin/main"

--- src/main/java/moine/config/kafka/KafkaProcessor.java
+++ src/main/java/moine/config/kafka/KafkaProcessor.java
@@ -7,9 +7,9 @@ import org.springframework.messaging.SubscribableChannel;
 
 public interface KafkaProcessor {
     // 발행, 구독 토픽 이름 설정
-    String INPUT = "event-in"; // 구독하는 토픽
-    String OUTPUT = "event-out";
-//    String OUTPUT1 = "authTopic"; // 발행할 토픽
+    String INPUT = "myTopic"; // 구독하는 토픽
+//    String OUTPUT = "event-out";
+    String OUTPUT1 = "myTopic"; // 발행할 토픽
 
     @Input(INPUT)
     SubscribableChannel inboundTopic();
