21f4dcc86bbce498b1f98da4000432d66212d6f8
KimRiun <kimriun27@gmail.com>
2022-09-02 17:18:09 +0900

    feat: middle 서버와 연결

--- src/main/java/moine/infra/controller/AuthController.java
+++ src/main/java/moine/infra/controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @RestController
-
 @RequiredArgsConstructor
 @RequestMapping(value="/auth")
 public class AuthController {
